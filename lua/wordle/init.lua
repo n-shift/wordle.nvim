493fa9e315512c3157809b8df07b2ff1486671e2
max397574 <max.heinzer397@gmx.ch>
2022-02-07 17:54:48 +0100

    feat(wordle): added some basic functions

--- lua/wordle/init.lua
+++ lua/wordle/init.lua
@@ -1,3 +1,10 @@
+-- TODO: check how many letters are input
+-- call functions to check
+-- color stuff by going through status
+-- map <BS> to delete character
+-- display input in float
+local wordle_buf
+local wordle_win
 --- Current time table
 local time = os.date("!*t")
 
@@ -27,3 +34,61 @@ file:close()
 local word = lines[index]
 
 print(word)
+
+local wordle = {}
+
+local input = {}
+
+local status = {
+    "wunused",
+    "wunused",
+    "wunused",
+    "unused",
+    "unused",
+}
+
+local function check()
+    for idx, letter in ipairs(input) do
+        if word[idx] == letter then
+            status[idx] = "correct"
+        elseif string.find(word, letter) then
+            status[idx] = "misplaced"
+        else
+            status[idx] = "unused"
+        end
+    end
+end
+
+local alphabet = vim.split("abcdefghijklmnopqrstuvwxyz", "")
+
+wordle.input = function(letter)
+    table.insert(input, letter)
+end
+
+wordle.play = function()
+    vim.api.nvim_buf_set_lines(wordle_buf, 0, -1, true, {})
+    vim.api.nvim_buf_set_option(wordle_buf, "bufhidden", "wipe")
+    local width = vim.api.nvim_win_get_width(0)
+
+    wordle_win = vim.api.nvim_open_win(wordle_buf, true, {
+        relative = "win",
+        win = 0,
+        width = math.floor(width * 0.9),
+        height = 28,
+        col = 1,
+        row = 1,
+        border = "shadow",
+        style = "minimal",
+    })
+    for _, char in ipairs(alphabet) do
+        vim.api.nvim_buf_set_keymap(
+            wordle_buf,
+            "n",
+            char,
+            "<cmd>lua require'wordle'.input(" .. '"' .. char .. '"' .. ")<CR>",
+            { noremap = true, silent = true }
+        )
+    end
+end
+
+return wordle
