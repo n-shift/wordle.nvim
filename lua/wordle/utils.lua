62bab410748f80d272bb8f3b7a43d5674dc00b47
shift-d <shiftkeydeveloper@gmail.com>
2022-02-13 13:02:53 +0300

    cursor moving

--- lua/wordle/utils.lua
+++ lua/wordle/utils.lua
@@ -9,4 +9,13 @@ function utils.julian(timestamp)
     return timestamp / 86400 + 2440587.5
 end
 
+function utils.cursor(wordle_win, attempt, letter)
+    if letter == 0 then letter = 1 end
+    print(wordle_win, attempt, letter)
+    local row = attempt*4-2
+    -- TODO: use byte-index
+    local col = 11*letter-6
+    vim.api.nvim_win_set_cursor(wordle_win, {row, col})
+end
+
 return utils
